f122b2a4b7bbebe4c2531adee87087da4802395c
118dg <54856465+118dg@users.noreply.github.com>
2021-09-14 04:14:01 +0900

    [맵과 셋] 9월 7일 - Update

--- 9월 7일 - 맵과 셋/1764.cpp
+++ 9월 7일 - 맵과 셋/1764.cpp
@@ -2,42 +2,30 @@
 //김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
 
 #include <iostream>
-#include <string>
+#include <map>
+#include <set>
 #include <algorithm>
-#include <cstring>
-#include <vector>
+
 using namespace std;
 
-int main(int argc, char* argv[])
+int main()
 {
 	int n, m;
 	string s;
-	vector<string> v1;
-	vector<string> v2;
+	map<string, int> heard;
+	set<string> seen;
 
 	cin >> n >> m;
 
-	v1.resize(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> v1[i];
+	for (int i = 0; i < n + m; ++i) {
+		string val;
+		cin >> val;
+		heard[val]++;
+		if (heard[val] > 1) seen.insert(val);
 	}
 
-	sort(v1.begin(), v1.end());
-
-	for (int i = 0; i < m; ++i) {
-		cin >> s;
-
-		if (binary_search(v1.begin(), v1.end(), s)) {
-			v2.push_back(s);
-		}
-	}
-
-	sort(v2.begin(), v2.end());
-
-	cout << v2.size() << endl;
-	for (int i = 0; i < v2.size(); ++i) {
-		cout << v2[i] << endl;
-	}
+	cout << seen.size() << "\n";
+	for (auto i : seen) cout << i << "\n";
 
 	return 0;
 }
